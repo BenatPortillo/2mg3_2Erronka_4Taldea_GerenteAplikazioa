248d3deb540d7b6b14b406acb0ecccbae7dd6d11
Beñat Portillo <ben_por_ech@goierrieskola.org>
2025-03-05 14:45:19 +0100

    eguraldia ongi

--- eguraldiancommit.bat
+++ eguraldiancommit.bat
@@ -3,9 +3,15 @@ cd /d ../2mg3_2Erronka_4Taldea_eguraldia/
 
 git checkout master
 
+
+git pull origin master
+
+
 git add .
 
+
 git commit -m "Eguraldia aktualizatuta automatikoki"
+
 
 git push origin master
 
